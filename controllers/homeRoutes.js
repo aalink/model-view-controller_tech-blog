6c147a0f3f8e1acd0dfcddf5cb3a049355cce453
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-06-22 16:54:11 -0400

    Rewriting get dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -32,29 +32,46 @@ router.get("/", async (req, res) => {
 router.get("/dashboard", withAuth, async (req, res) => {
   //router.get('/dashboard', async (req, res) => {
   try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ["password"] },
-      include: [{ model: Post }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    console.log("user", user);
-
-    res.render("dashboard", {
-      ...user,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
+    const postData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id
+      }
+    })
+    const posts = postData.map((post)=>{
+      post.get({plain: true})
+    })
+    res.render("dashboard", posts)
+  } catch (error) {
+    res.redirect("login")
   }
+  ////////////////////////////////////////////////////////////////
+  // USED TO BE THIS CODE ///////////////////////////////////////
+  // try {
+  //   // Find the logged in user based on the session ID
+  //   const userData = await User.findByPk(req.session.user_id, {
+  //     attributes: { exclude: ["password"] },
+  //     include: [{ model: Post }],
+  //   });
+
+  //   const user = userData.get({ plain: true });
+
+  //   console.log("user", user);
+
+  //   res.render("dashboard", {
+  //     ...user,
+  //     logged_in: req.session.logged_in,
+  //   });
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
+  ////////////////////////////////////////////////////////////////
+
 });
 
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect("/profile");
+    res.redirect("/dashboard");
     return;
   }
 
