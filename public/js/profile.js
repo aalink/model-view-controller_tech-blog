a6d011c0a857ae207012691a5c5a6c8da6fe95ee
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-06-23 22:02:24 -0400

    Changes name/description to title/content

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,13 +3,13 @@ const newFormHandler = async (event) => {
   event.preventDefault();
   console.log("SAVE POST BUTTON CLICKED")
 
-  const name = document.querySelector('#create-post-title').value.trim();
-  const description = document.querySelector('#create-post-content').value.trim();
+  const title = document.querySelector('#create-post-title').value.trim();
+  const content = document.querySelector('#create-post-content').value.trim();
 
-  if (name && description) {
+  if (title && content) {
     const response = await fetch(`/api/post`, {
       method: 'POST',
-      body: JSON.stringify({ name, description }),
+      body: JSON.stringify({ title, content }),
       headers: {
         'Content-Type': 'application/json',
       },
